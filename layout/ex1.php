2648222da80f1a1603617cd1b63ba28cfa8985ac
shammi22000 <shammi22000@student.hamk.fi>
2023-02-01 11:21:37 +0200

    ex1 modification

--- layout/ex1.php
+++ layout/ex1.php
@@ -18,6 +18,44 @@ include("header.php");?>
     <?php
     echo "<h3>",date("d.m.Y"),"</h3>";
     ?>
+<h3>3.4 Variable in php</h3>
+    <?php
+    $title1="PHP is interesting";
+    echo "<h1>".$title."</h1>";
+    ?>
+<h3>3.5 Tables and Variables</h3>
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+
+echo "
+<table>
+    <tr>
+        <th>S.N</th>
+        <th>Name</th>
+        <th>Grade</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Pekka</td>
+        <td>$g1</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Johanna</td>
+        <td>$g2</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>John</td>
+        <td>$g3</td>
+    </tr>
+</table>"
+?>
+
+<h3>4 screenshot of development environment</h3>
+<img src="envirnImage" alt="Environment image">
 </body>
 
 <?php include("footer.php");?>
